831d1f71447120319046607d259e09084a63aca1
Alejandro Alonso <21010777+Alexalonso04@users.noreply.github.com>
2020-04-20 14:08:53 -0400

    robot moves with the basic configuration

--- StudenRobot/SampleStudentBot.cs
+++ StudenRobot/SampleStudentBot.cs
@@ -2,7 +2,22 @@
 
 namespace CAP4053.Student
 {
-    public class SampleStudentBot : TeamRobot
-    {
+    public class SampleStudentBot : TeamRobot {
+        override
+        public void Run()
+        {
+            while(true)
+            {
+                Ahead(100);
+                TurnGunRight(360);
+                Back(100);
+                TurnGunRight(360);
+            }
+        }
+
+        override
+        public void OnScannedRobot(ScannedRobotEvent e) {
+            Fire(100);
+        }
     }
 }
